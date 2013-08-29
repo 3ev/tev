d8e2301663f65463a9d85046bc5c1793b1ae70fc
Ben Constable <ben@benconstable.co.uk>
2013-08-29 16:10:15 +0100

    Further changes to menu view helper

--- Classes/ViewHelpers/Page/MenuViewHelper.php
+++ Classes/ViewHelpers/Page/MenuViewHelper.php
@@ -17,12 +17,22 @@ class Tx_Tev_ViewHelpers_Page_MenuViewHelper
     public function initializeArguments()
     {
         parent::initializeArguments();
+
         $this->registerArgument(
             'classItem',
             'string',
             'Optional extra class to add to each link (and wrapper) element. Use ' .
             ':level in the class name to add the current level number to the class.'
         );
+
+        for ($i = 2; $i <= 10; $i++) {
+            $this->registerArgument(
+                'classItem' . $i,
+                'string',
+                'Optional extra class to add to each link (and wrapper) element (level ' . $i . '. Use ' .
+                ':level in the class name to add the current level number to the class.'
+            );
+        }
     }
 
     /**
@@ -35,12 +45,40 @@ class Tx_Tev_ViewHelpers_Page_MenuViewHelper
      */
     protected function autoRender($menu, $level = 1)
     {
-        if ($ci = $this->arguments['classItem']) {
+        $className = 'classItem' . ($level > 1 ? $level : '');
+
+        if ($this->arguments[$className] !== null) {
+            $ci = $this->arguments[$className];
+        } else {
+            $ci = $this->arguments['classItem'];
+        }
+
+        if ($ci) {
             foreach ($menu as &$page) {
                 $page['class'] .= ' ' . str_replace(':level', $level, $ci);
             }
         }
 
         return parent::autoRender($menu, $level);
+    }
+
+    /**
+     * Fix error with hidden pages not showing correctly.
+     *
+     * @see Tx_Vhs_ViewHelpers_Page_Menu_Abstract_MenuViewHelper
+     * @param array $page
+     * @param array $rootLine
+     * @return array
+     */
+    protected function getMenuItemEntry($page, $rootLine)
+    {
+        $page = parent::getMenuItemEntry($page, $rootLine);
+        if (!$this->arguments['showHidden']) {
+            $addWhere = 'AND nav_hide=0';
+        } else {
+            $addWhere = '';
+        }
+        $page['hasSubPages'] = (count($this->pageSelect->getMenu($page['uid'], $fields = '*', $sortField = 'sorting', $addWhere)) > 0) ? 1 : 0;
+        return $page;
     }
 }
