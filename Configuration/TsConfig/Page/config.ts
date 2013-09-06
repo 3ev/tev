761cb9554b3124c04344cf0100c817591fc05e3b
Ben Constable <ben@benconstable.co.uk>
2013-09-06 16:32:42 +0100

    Remove unwanted fields from Fluidcontent elements, bump version

---
+++ Configuration/TsConfig/Page/config.ts
@@ -0,0 +1,15 @@
+###################################################
+# Configuration
+###################################################
+
+TCEFORM.tt_content {
+    header.types.fluidcontent_content.disabled = 1
+    header_layout.types.fluidcontent_content.disabled = 1
+    header_position.types.fluidcontent_content.disabled = 1
+    date.types.fluidcontent_content.disabled = 1
+    header_link.types.fluidcontent_content.disabled = 1
+    layout.types.fluidcontent_content.disabled = 1
+    spaceBefore.types.fluidcontent_content.disabled = 1
+    spaceAfter.types.fluidcontent_content.disabled = 1
+    section_frame.types.fluidcontent_content.disabled = 1
+}
--- ext_tables.php
+++ ext_tables.php
@@ -2,10 +2,15 @@
 
 if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
+// Add Page TS Config
+$pageTsConfig = \TYPO3\CMS\Core\Utility\GeneralUtility::getUrl(
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Configuration/TsConfig/Page/config.ts');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig($pageTsConfig);
+
 // Add config for RealURL fixedPostVars
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', array(
     'tx_tev_postvars' => array(
-        'exclude' => 1,     
+        'exclude' => 1,
         'label' => 'LLL:EXT:tev/Resources/Private/Language/locallang.xml:pages.tx_tev_postvars',
         'config' => array(
             'type' => 'input',
