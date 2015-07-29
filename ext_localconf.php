e2e8b1a1683af8fa21c8f8969419a0f60ff1a84f
Muthuswamy Kumaresan <muthu@3ev.com>
2015-07-29 09:17:16 +0000

    Fix errors related to loading typoscripts

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,10 +3,10 @@
 if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
 // Automatically include TypoScript dependencies
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('EXT:css_styled_content/static');
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('EXT:fluidpages/Configuration/TypoScript');
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('EXT:fluidcontent/Configuration/TypoScript');
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('EXT:tev/Configuration/TypoScript');
+\FluidTYPO3\Flux\Core::addStaticTypoScript('EXT:css_styled_content/static');
+\FluidTYPO3\Flux\Core::addStaticTypoScript('EXT:fluidpages/Configuration/TypoScript');
+\FluidTYPO3\Flux\Core::addStaticTypoScript('EXT:fluidcontent/Configuration/TypoScript');
+\FluidTYPO3\Flux\Core::addStaticTypoScript('EXT:tev/Configuration/TypoScript');
 
 // Hook into RealURL config generation
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration']['tev'] = 'Tev\\Tev\\Hook\\RealUrlAutoConfigurationHook->updateConfig';
