f66a6992fa11678f97921b04b09ea7aab8bce66b
Muthuswamy Kumaresan <muthu@3ev.com>
2015-07-29 09:38:04 +0000

    Revert back to v1.3.0

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,10 +3,10 @@
 if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
 // Automatically include TypoScript dependencies
-\FluidTYPO3\Flux\Core::addStaticTypoScript('EXT:css_styled_content/static');
-\FluidTYPO3\Flux\Core::addStaticTypoScript('EXT:fluidpages/Configuration/TypoScript');
-\FluidTYPO3\Flux\Core::addStaticTypoScript('EXT:fluidcontent/Configuration/TypoScript');
-\FluidTYPO3\Flux\Core::addStaticTypoScript('EXT:tev/Configuration/TypoScript');
+Tx_Flux_Core::addGlobalTypoScript('EXT:css_styled_content/static');
+Tx_Flux_Core::addGlobalTypoScript('EXT:fluidpages/Configuration/TypoScript');
+Tx_Flux_Core::addGlobalTypoScript('EXT:fluidcontent/Configuration/TypoScript');
+Tx_Flux_Core::addGlobalTypoScript('EXT:tev/Configuration/TypoScript');
 
 // Hook into RealURL config generation
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration']['tev'] = 'Tev\\Tev\\Hook\\RealUrlAutoConfigurationHook->updateConfig';
