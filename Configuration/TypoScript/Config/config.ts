b0b76a50a60b88b5461168b42f71c6d87b3144d0
Ben Constable <ben@benconstable.co.uk>
2015-08-05 12:16:49 +0100

    Add some additional Typoscript to config

--- Configuration/TypoScript/Config/config.ts
+++ Configuration/TypoScript/Config/config.ts
@@ -3,49 +3,71 @@
 ###################################################
 
 config {
+
     # Disable stdwrap comments
+
     disablePrefixComment = 1
 
     # Set UTF-8
-    renderCharset = utf-8
 
-    # Set UTF-8
+    renderCharset = utf-8
     metaCharset = utf-8
 
-    # HTML5
+    # Set Doctype to HTML5
+
     doctype = html5
 
-    # Remove XHTML stuff
+    # Remove XHTML junk
+
     xmlprologue = none
 
     # Set HTML language from current system language
+
     htmlTag_setParams = lang="{$config.language}"
 
     # Enable RealURL
+
     tx_realurl_enable = 1
 
     # Caching
+
     sendCacheHeaders = 1
 
     # Remove header comment
+
     headerComment >
 
     # Disable image borders
+
     disableImgBorderAttr = 1
 
     # Remove default CSS
+
     removeDefaultCss = 1
 
     # Remove default JS
+
     removeDefaultJS = 1
 
     # Concat CSS automatically
+
     concatenateCss = 1
 
     # Concat JS automatically
+
     concatenateJs = 1
 
     # Enable TypoLink across domains
+
     typolinkCheckRootline = 1
     typolinkEnableLinksAcrossDomains = 1
+
+    # Remove the TS generated page title, so that we can use the VHS view helper
+    # instead
+
+    noPageTitle = 2
+
+    # Prefix all URLs so they start at the root
+
+    absRefPrefix = /
 }
