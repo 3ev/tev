84aba6a4bade2c7a99b70f8429b88e629be8ae46
Ben Constable <ben@benconstable.co.uk>
2015-08-05 12:13:55 +0100

    Include up to date CSS Styled Content TS

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,14 +1,19 @@
 <?php
 
-if (!defined ('TYPO3_MODE')) die ('Access denied.');
+if (!defined ('TYPO3_MODE')) {
+    die ('Access denied.');
+}
 
 // Automatically include TypoScript dependencies
-FluidTYPO3\Flux\Core::addStaticTypoScript('EXT:css_styled_content/static/v6.2');
-FluidTYPO3\Flux\Core::addStaticTypoScript('EXT:tev/Configuration/TypoScript');
+
+\FluidTYPO3\Flux\Core::addStaticTypoScript('EXT:css_styled_content/static/');
+\FluidTYPO3\Flux\Core::addStaticTypoScript('EXT:tev/Configuration/TypoScript');
 
 // Hook into RealURL config generation
+
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration']['tev'] = 'Tev\\Tev\\Hook\\RealUrlAutoConfigurationHook->updateConfig';
 
 // Add new cache type to clear RealURL config
+
 $TYPO3_CONF_VARS['SC_OPTIONS']['additionalBackendItems']['cacheActions'][] = 'Tev\\Tev\\Url\\CacheMenu';
 $TYPO3_CONF_VARS['BE']['AJAX']['tx_tev::clearcacheurl'] = 'Tev\\Tev\\Url\\Cache->clear';
