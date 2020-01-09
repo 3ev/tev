aba221447a2250e83b36baf20b300361c4197b98
aaron-grover <30999889+aaron-grover@users.noreply.github.com>
2020-01-09 09:00:05 +0000

    Docs and added new array_key_exists_recursive func

--- Classes/Util/Misc.php
+++ Classes/Util/Misc.php
@@ -10,9 +10,39 @@ namespace Tev\Tev\Util;
  */
 class Misc
 {
+    /**
+    * Returns needle position if a string contains a particular substring
+    * 
+    * @param string $needle
+    * @param string $haystack
+    *
+    * @return mixed
+    */
     public function str_contains($needle, $haystack)
     {
         return (strpos($haystack, $needle) !== false);
+    }
+    
+    /**
+    * Recursively checks a multidimensional array to see if a key exists, returnign it if it does
+    *
+    * @param string $needle
+    * @param array $haystack
+    *
+    * @return mixed
+    */
+    function array_key_exists_recursive($needle, $haystack)
+    {
+        $result = array_key_exists($needle, $haystack);
+        if ($result) return $result;
+
+        foreach ($haystack as $v) {
+            if (is_array($v)) {
+                $result = array_key_exists_recursive($needle, $v);
+            }
+            if ($result) return $result;
+        }
+        return $result;
     }
 }
 
